f0ffe51c16733502b6484cbac55d5042fac42770
Бабышев Максим <cmdazaza@gmail.com>
2021-07-11 17:05:44 +0300

    Now releases made on main branch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import py2exe
 
 setup(console=['main.py'],
       name="InversionFilterManager",
-      version="0.0.1",
+      version="0.0.2",
       description="Inverts colors when you opens blinding white windows",
       data_files=[('.', ["config_description.ini"])]
       )
