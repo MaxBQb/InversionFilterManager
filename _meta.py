2b66cc6c61528bd857ce4f8bd1d4724b7014eb3c
Бабышев Максим <cmdazaza@gmail.com>
2021-09-04 11:35:43 +0300

    Release 0.5.0
    Breaking changes!
    
    ~ Replace config.ini with settings.yaml
    + Reload from disk option in SystemTray
    ~ Now config dumped only when corrupted or not found
    + Settings for update checkups intervals
    + Choose window dialog now appears only when needed
    ~ Enum fields shows correct
    + Check before creation if rule with same name is already defined
    + Rules changes now applies immediately
    + Now only one instance of this app may exist
    + Try to setup filter options at startup
    + Settings for color filter setup

--- _meta.py
+++ _meta.py
@@ -2,7 +2,7 @@ import sys
 from enum import Enum, auto
 
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __product_name__ = "InversionFilterManager"
 __author__ = "MaxBQb"
 __icon__ = "img/inversion_manager.ico"
