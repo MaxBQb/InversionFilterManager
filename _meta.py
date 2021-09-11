e326e24cb748a5cddb3329046e45a33d84665892
Бабышев Максим <cmdazaza@gmail.com>
2021-09-11 15:58:47 +0300

    Release 0.5.2
    + Remember pid option
    + Admin mode indicator
    ~ Fix: Rule name placeholder sometimes contains .exe

--- _meta.py
+++ _meta.py
@@ -2,7 +2,7 @@ import sys
 from enum import Enum, auto
 
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __product_name__ = "InversionFilterManager"
 __author__ = "MaxBQb"
 __icon__ = "img/inversion_manager.ico"
