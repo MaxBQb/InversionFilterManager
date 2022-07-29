bebe74124a50ef41161e8e7ff7739965eacff5c2
Бабышев Максим <cmdazaza@gmail.com>
2022-07-29 12:05:46 +0300

    Remove dirty fix for pystray

--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
-import os.path
 from distutils.core import setup
 from glob import glob
+
+# noinspection PyUnresolvedReferences,PyPackageRequirements
 import py2exe
+
 import _meta as app
-
-
-def dirty_pystray_fix():
-    from pystray import __path__ as tray_path, __package__ as tray_package
-    tray_path = os.path.join(tray_path[0], "__init__.py")
-
-    with open(tray_path, "r", encoding="utf-8") as f:
-        data = f.readlines()
-
-    if not data[0].startswith('__package__'):
-        with open(tray_path, "w", encoding="utf-8") as f:
-            f.writelines([f"__package__ = '{tray_package}'\n"] + data)
-
-
-# Pystray can't find __package__ after py2exe'fication
-# Okay.. if someone have clean solution, leave comment plz
-dirty_pystray_fix()
-
 
 setup(console=[dict(
         script='main.py',
